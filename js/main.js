a402e8dd3088a6a2d4d0d235efd3cd8e87e8be27
EnigmaSn <enigmasnape@gmail.com>
2017-04-10 20:18:10 +0300

    1470

--- js/main.js
+++ js/main.js
@@ -2,4 +2,33 @@
 $('.hamburger-menu').on('click', function() {
   $('.bar').toggleClass('animate');
   $('.menu').toggleClass('open');
-})
+});
+
+// ОТКРЫТИЕ МЕНЮ
+var menu_button = document.querySelector(".hamburger-menu");
+var menu = document.querySelector(".menu");
+
+// отлавливает событие
+menu_button.addEventListener("click", function(event) {
+  if (menu.classList.contains(".open")) {
+    // отмена действия по умолчанию
+    event.preventDefault();
+    // вывод в консоль
+    console.log("клик таки");
+    // без точки, Карл, ибо этот метод работает только с классами
+    menu.classList.remove(".open");
+  }
+  else {
+    event.preventDefault();
+    menu.classList.add(".open");
+  }
+});
+
+// ЛИПКАЯ СТРЕЛКА В ХЕДЕРЕ
+$(document).ready(function(){
+  $("#sticker").sticky({
+    topSpacing: 0;
+    stopper: '#stopper';
+  });
+});
+
