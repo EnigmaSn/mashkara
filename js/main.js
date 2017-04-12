33c216b65173a3c866d4f789cb03b76ee92d73d6
EnigmaSn <enigmasnape@gmail.com>
2017-04-13 01:22:38 +0300

    скромный хедер

--- js/main.js
+++ js/main.js
@@ -1,45 +1,11 @@
-//MENU BUTTON
-$('.hamburger-menu').on('click', function() {
-  $('.bar').toggleClass('animate');
-  $('.menu').toggleClass('open');
-});
-
-// ОТКРЫТИЕ МЕНЮ
-var menu_button = document.querySelector(".hamburger-menu");
-var menu = document.querySelector(".menu");
-
-// отлавливает событие
-menu_button.addEventListener("click", function(event) {
-  if (menu.classList.contains(".open")) {
-    // отмена действия по умолчанию
-    event.preventDefault();
-    // вывод в консоль
-    console.log("клик таки");
-    // без точки, Карл, ибо этот метод работает только с классами
-    menu.classList.remove(".open");
-  }
-  else {
-    event.preventDefault();
-    menu.classList.add(".open");
-  }
-});
-
-// ЛИПКАЯ СТРЕЛКА В ХЕДЕРЕ
-$(document).ready(function(){
-  $("#sticker").sticky({
-    topSpacing: 0;
-    stopper: '#stopper';
-  });
-});
-
 // Хедер в мобильнике
 $(document).ready(function() {
 
-  var header = $(".page-header"); // Меню
-  var scrollPrev = 0; // Предыдущее значение скролла
+  var header = $(".page-header__mobile"); // Меню
+  var scrollPrev = 0 // Предыдущее значение скролла
 
   $(window).scroll(function() {
-    var scrolled = $(window).scrollTop('100px'); // Высота скролла в px
+    var scrolled = $(window).scrollTop(); // Высота скролла в px
     var firstScrollUp = false; // Параметр начала сколла вверх
     var firstScrollDown = false; // Параметр начала сколла вниз
 
@@ -96,5 +62,44 @@ $(document).ready(function() {
       // Присваеваем текущее значение скролла предыдущему
       scrollPrev = scrolled;
     }
+
   });
+
+
+
+
 });
+
+//MENU BUTTON
+$('.hamburger-menu').on('click', function() {
+  $('.bar').toggleClass('animate');
+  $('.menu').toggleClass('open');
+});
+
+// ОТКРЫТИЕ МЕНЮ
+var menu_button = document.querySelector(".hamburger-menu");
+var menu = document.querySelector(".menu");
+
+// отлавливает событие
+menu_button.addEventListener("click", function(event) {
+  if (menu.classList.contains(".open")) {
+    // отмена действия по умолчанию
+    event.preventDefault();
+    // вывод в консоль
+    console.log("клик таки");
+    // без точки, Карл, ибо этот метод работает только с классами
+    menu.classList.remove(".open");
+  }
+  else {
+    event.preventDefault();
+    menu.classList.add(".open");
+  }
+});
+
+// ЛИПКАЯ СТРЕЛКА В ХЕДЕРЕ
+// $(document).ready(function(){
+//   $("#sticker").sticky({
+//     topSpacing: 0;
+//     stopper: '#stopper';
+//   });
+// });
